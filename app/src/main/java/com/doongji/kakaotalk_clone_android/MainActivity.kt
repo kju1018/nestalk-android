e5ad19ae97d8246b9fe9bb1db4b5d964e2eabf24
KIM M S <61860897+kju1018@users.noreply.github.com>
2021-02-09 20:29:57 +0900

    feat: 메인액티비티
    메인액티비티 바텀네비게이션 구현
    바텀네비게이션 메뉴에 프래그먼트 연결

--- app/src/main/java/com/doongji/kakaotalk_clone_android/MainActivity.kt
+++ app/src/main/java/com/doongji/kakaotalk_clone_android/MainActivity.kt
@@ -2,10 +2,38 @@ package com.doongji.kakaotalk_clone_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.FragmentTransaction
+import com.doongji.kakaotalk_clone_android.databinding.ActivityMainBinding
+import com.doongji.kakaotalk_clone_android.ui.fragment.ChatFragment
+import com.doongji.kakaotalk_clone_android.ui.fragment.FriendFragment
+import com.doongji.kakaotalk_clone_android.ui.fragment.SettingFragment
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBinding: ActivityMainBinding
+    
+    private lateinit var fragmentTransaction:FragmentTransaction
+    private val friendFragment:FriendFragment by lazy {FriendFragment.newInstance()}
+    private val chatFragment:ChatFragment by lazy { ChatFragment.newInstance() }
+    private val settingFragment:SettingFragment by lazy { SettingFragment.newInstance() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        supportFragmentManager.beginTransaction().replace(R.id.fl_main_layout, friendFragment).commit()
+
+        activityMainBinding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            fragmentTransaction = supportFragmentManager.beginTransaction()
+            when(it.itemId){
+                R.id.item_friend -> { fragmentTransaction.replace(R.id.fl_main_layout, friendFragment).commit() }
+                R.id.item_chat -> { fragmentTransaction.replace(R.id.fl_main_layout, chatFragment).commit()}
+                R.id.item_setting -> {fragmentTransaction.replace(R.id.fl_main_layout, settingFragment).commit() }
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
+
     }
 }
